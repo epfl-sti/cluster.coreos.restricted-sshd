1169055dee19157b616ec0f495e8a35163c2f623
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-01 21:03:10 +0100

    Now with pluggable policy
    * New exported classes, Server and Policy
    * Server has .findPolicyByPubkey()
    * Policy has .debug(), an ID, and (gasp!) a pubkey as a buffer

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var docoptmd = require('docoptmd'),
+var fs = require('fs'),
+    docoptmd = require('docoptmd'),
     sshd = require('./sshd');
 
 var argv = process.argv.slice(process.argv[0].endsWith("/node") ? 2 : 1);
 var options = docoptmd(__dirname, {argv: argv});
 
 var sshdPort = options["--port"];
-var server = sshd.startServer(sshdPort, function() {
-    console.log("Running on port " + this.address().port);
-});
+var server = new sshd.Server({
+        privateKey: fs.readFileSync('tmpkeys/host_rsa_key'),
+    });
+
+var buffersEqual = require('buffer-equal-constant-time'),
+    ssh2 = require('ssh2'),
+    utils = ssh2.utils;
+var pubKey = utils.genPublicKey(utils.parseKey(fs.readFileSync('tmpkeys/user.pub')));
+
+server.findPolicyByPubkey = function (key) {
+    if (key.algo === pubKey.fulltype
+        && buffersEqual(key.data, pubKey.public)) {
+        var policy = new sshd.Policy("MAGIC PUBKEY");
+        policy.publicOrig = pubKey.publicOrig;  // OW OW OUCH BLEAAARGH
+        return policy;
+    }
+};
+
+server.listen({port: sshdPort},
+    function() {
+        console.log("Running on port " + this.address().port);
+    });
--- sshd.js
+++ sshd.js
@@ -2,45 +2,73 @@
  * sshd entry point, using based on https://github.com/mscdex/ssh2
  */
 
-var fs = require('fs'),
+var assert = require('assert'),
     crypto = require('crypto'),
-    inspect = require('util').inspect;
-var buffersEqual = require('buffer-equal-constant-time'),
-    ssh2 = require('ssh2'),
-    utils = ssh2.utils;
+    inspect = require('util').inspect,
+    debug = require('debug')('sshd'),
+    ssh2 = require('ssh2');
 
-var pubKey = utils.genPublicKey(utils.parseKey(fs.readFileSync('tmpkeys/user.pub')));
+/**
+ * A policy object.
+ *
+ * Methods decide what happens in various circumstances; they should be
+ * overridden.
+ *
+ * @constructor
+ */
+var Policy = exports.Policy = function (id) {
+    this.debug = function(msg) { debug(id + ": " + msg); };
 
-exports.startServer = function(port, opt_listenAddress, done) {
-    if (done === undefined) {
-        done = opt_listenAddress;
-        opt_listenAddress = '';
-    }
-        
-    return new ssh2.Server({
-        privateKey: fs.readFileSync('tmpkeys/host_rsa_key')
-    }, function(client) {
-        console.log('Client connected!');
+    this.grantFleetSock = function (stream) {
+        stream.stderr.write('Oh no, the dreaded errors!\n');
+        stream.write('Just kidding about the errors!\n');
+        stream.exit(0);
+        stream.end();
+    };
+};
 
-        client.on('authentication', function(ctx) {
-            if (ctx.method === 'password'
-                && ctx.username === 'foo'
-                && ctx.password === 'bar')
-                ctx.accept();
-            else if (ctx.method === 'publickey'
-                && ctx.key.algo === pubKey.fulltype
-                && buffersEqual(ctx.key.data, pubKey.public)) {
-                if (ctx.signature) {
-                    var verifier = crypto.createVerify(ctx.sigAlgo);
-                    verifier.update(ctx.blob);
-                    if (verifier.verify(pubKey.publicOrig, ctx.signature, 'binary'))
-                        ctx.accept();
-                    else
+/**
+ * Server class.
+ *
+ * The server does nothing by default; some of its methods must be overridden.
+ *
+ * @param options Options dict
+ * @param options.privateKey The server's private key as text
+ * @constructor
+ */
+var Server = exports.Server = function (options) {
+    var self = this;
+
+    server = new ssh2.Server({
+        privateKey: options.privateKey
+    }, function (client) {
+        debug('New client connection');
+
+        client.on('authentication', function (ctx) {
+            if (ctx.method === 'publickey') {
+                if (! client.policy) {
+                    client.policy = self.findPolicyByPubkey(ctx.key);
+                    if (! client.policy) {
+                        debug("Presented unacceptable key");
                         ctx.reject();
-                } else {
+                        return;
+                    }
+                }
+                if (! ctx.signature) {
                     // if no signature present, that means the client is just checking
                     // the validity of the given public key
                     ctx.accept();
+                } else {
+                    var verifier = crypto.createVerify(ctx.sigAlgo);
+                    verifier.update(ctx.blob);
+                    if (verifier.verify(client.policy.publicOrig,
+                            ctx.signature, 'binary')) {
+                        client.policy.debug("Public key authentication successful");
+                        ctx.accept();
+                    } else {
+                        client.policy.debug("Failed pubkey authentication");
+                        ctx.reject();
+                    }
                 }
             } else if (ctx.method === 'none') {
                 // Client wants list of authentication methods
@@ -48,22 +76,83 @@ exports.startServer = function(port, opt_listenAddress, done) {
                 ctx.reject(["publickey"]);
             } else
                 ctx.reject();
-        }).on('ready', function() {
-            console.log('Client authenticated!');
+        });
+        client.on('ready', function () {
+            client.policy.debug('Client authenticated!');
 
-            client.on('session', function(accept, reject) {
+            client.on('session', function (accept, reject) {
+                client.policy.debug('Client requests a session');
+
                 var session = accept();
-                session.once('exec', function(accept, reject, info) {
-                    console.log('Client wants to execute: ' + inspect(info.command));
-                    var stream = accept();
-                    stream.stderr.write('Oh no, the dreaded errors!\n');
-                    stream.write('Just kidding about the errors!\n');
-                    stream.exit(0);
-                    stream.end();
+                session.once('exec', function (accept, reject, info) {
+                    client.policy.debug('Client wants to execute: ' +
+                        inspect(info.command));
+                    if (info.command.indexOf("fleetctl fd-forward") > -1 &&
+                        info.command.indexOf("fleet.sock") > -1) {
+                        client.policy.debug("Routing to restricted fleet.sock");
+                        var stream = accept();
+                        client.policy.grantFleetSock(stream);
+                    } else {
+                        client.policy.debug("Unhandled command: " +
+                            inspect(info.command));
+                        reject();
+                    }
+                });
+                session.on('subsystem', function (accept, reject, info) {
+                    client.policy.debug('Client invokes a subsystem');
+                    reject();
+                });
+                session.on('auth-agent', function (accept, reject, info) {
+                    client.policy.debug('Client wants to forward agent');
+                    accept();
                 });
             });
-        }).on('end', function() {
-            console.log('Client disconnected');
         });
-    }).listen(port, opt_listenAddress, done);
+        client.on('tcpip', function (accept, reject, info) {
+            client.policy.debug('Client wants to forward TCP');
+            reject();
+        });
+        client.on('openssh.streamlocal', function (accept, reject, info) {
+            client.policy.debug('Client wants to forward a stream');
+            reject();
+        });
+        client.on('end', function () {
+            if (client.policy) {
+                client.policy.debug('Client disconnected');
+            } else {
+                debug('Unauthenticated client disconnected');
+            }
+        });
+    });
+
+    self.listen = server.listen.bind(server);
+
+    /**
+     * Overridable method: find a policy for a given public key
+     * @param key
+     * @returns Policy instance, or undefined
+     */
+    self.findPolicyByPubkey = function (key) {};
+};
+
+/**
+ * Create and .listen()s a Server and return it.
+ *
+ * @param opt_options An options dict (optional; passed to constructor)
+ * @param opt_options.listenAddress The local IP address to listen on; default ''
+ * @param opt_options.port The server port (picked automatically by default)
+ * @param done Callback invoked when the server socket is ready
+ * @returns {Server}
+ */
+exports.startServer = function(opt_options, done) {
+    if (done === undefined) {
+        done = opt_options;
+        opt_options = {};
+    }
+    var server = new Server(opt_options);
+    server.listen(opt_options.port === undefined ? 0 : opt_options.port,
+        opt_options.listenAddress === undefined ? '': opt_options.listenAddress,
+        done);
+
+    return server;
 };
