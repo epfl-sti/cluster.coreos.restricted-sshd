cf55a8b3cbf25ed14a13843294fbe9a103dd0923
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-01 17:48:14 +0100

    Now responds to SSH protocol!

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var docoptmd = require('docoptmd');
+var docoptmd = require('docoptmd'),
+    sshd = require('./sshd');
 
 var argv = process.argv.slice(process.argv[0].endsWith("/node") ? 2 : 1);
 var options = docoptmd(__dirname, {argv: argv});
 
-console.log("Running on port " + options["--port"]);
+var sshdPort = options["--port"];
+var server = sshd.startServer(sshdPort, function() {
+    console.log("Running on port " + this.address().port);
+});
---
+++ sshd.js
@@ -0,0 +1,65 @@
+/**
+ * sshd entry point, using based on https://github.com/mscdex/ssh2
+ */
+
+var fs = require('fs'),
+    crypto = require('crypto'),
+    inspect = require('util').inspect;
+var buffersEqual = require('buffer-equal-constant-time'),
+    ssh2 = require('ssh2'),
+    utils = ssh2.utils;
+
+var pubKey = utils.genPublicKey(utils.parseKey(fs.readFileSync('tmpkeys/user.pub')));
+
+exports.startServer = function(port, opt_listenAddress, done) {
+    if (done === undefined) {
+        done = opt_listenAddress;
+        opt_listenAddress = '';
+    }
+        
+    return new ssh2.Server({
+        privateKey: fs.readFileSync('tmpkeys/host_rsa_key')
+    }, function(client) {
+        console.log('Client connected!');
+
+        client.on('authentication', function(ctx) {
+            if (ctx.method === 'password'
+                && ctx.username === 'foo'
+                && ctx.password === 'bar')
+                ctx.accept();
+            else if (ctx.method === 'publickey'
+                && ctx.key.algo === pubKey.fulltype
+                && buffersEqual(ctx.key.data, pubKey.public)) {
+                if (ctx.signature) {
+                    var verifier = crypto.createVerify(ctx.sigAlgo);
+                    verifier.update(ctx.blob);
+                    if (verifier.verify(pubKey.publicOrig, ctx.signature, 'binary'))
+                        ctx.accept();
+                    else
+                        ctx.reject();
+                } else {
+                    // if no signature present, that means the client is just checking
+                    // the validity of the given public key
+                    ctx.accept();
+                }
+            } else
+                ctx.reject();
+        }).on('ready', function() {
+            console.log('Client authenticated!');
+
+            client.on('session', function(accept, reject) {
+                var session = accept();
+                session.once('exec', function(accept, reject, info) {
+                    console.log('Client wants to execute: ' + inspect(info.command));
+                    var stream = accept();
+                    stream.stderr.write('Oh no, the dreaded errors!\n');
+                    stream.write('Just kidding about the errors!\n');
+                    stream.exit(0);
+                    stream.end();
+                });
+            });
+        }).on('end', function() {
+            console.log('Client disconnected');
+        });
+    }).listen(port, opt_listenAddress, done);
+};
